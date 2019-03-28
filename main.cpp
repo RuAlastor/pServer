fc328bd66b27d2da27d27425fcaead41715c62fb
Sorokin Eugene <RuAlastor@yandex.ru>
2019-03-28 15:41:38 +0300

    Set up socket for listening

--- main.cpp
+++ main.cpp
@@ -1,8 +1,26 @@
-#include <QCoreApplication>
+#include <iostream>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
 
-int main(int argc, char *argv[])
-{
-    QCoreApplication a(argc, argv);
+#define HAVE_STRUCT_TIMESPEC
+#include <pthread.h>
 
-    return a.exec();
+typedef int type_socket;
+
+int main() {
+    // Socket init
+    type_socket listening_socket = socket(AF_INET, SOCK_STREAM, 0);
+    // Setting socket info
+    struct sockaddr_in* socket_info = new sockaddr_in;
+    socket_info->sin_family = AF_INET;
+    socket_info->sin_port = htons(14888);
+    socket_info->sin_addr.s_addr = htonl(INADDR_LOOPBACK); // 127.0.0.1
+    // Binding listening socket
+    bind(listening_socket, (struct sockaddr *)socket_info, sizeof(socket_info));
+    // Setting listening socket to listening
+    listen(listening_socket, SOMAXCONN);
+
+
+    return 0;
 }
