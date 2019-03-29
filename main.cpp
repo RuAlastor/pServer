19ad7ad72e4a58a8f08d23662523c1b378d94b29
Sorokin Eugene <RuAlastor@yandex.ru>
2019-03-29 15:36:16 +0300

    Fixed pointer

--- main.cpp
+++ main.cpp
@@ -5,22 +5,24 @@ int main(int argc, char* argv[]) {
     try {
         // Socket initialization
         type_socket master_socket = socket(AF_INET, SOCK_STREAM, 0);
-                                            // IPv4     gTCP     IPPROTO_TCP
+                                            // IPv4     TCP     IPPROTO_TCP
         if ( master_socket == -1 ) {
             throw Error_types::Init_error();
         }
         std::cout << "Listening socket has been opened!\n";
 
         // Setting socket info
-        struct sockaddr_in socket_info;
-        socket_info.sin_family = AF_INET;
-        socket_info.sin_port = htons(12345); // Change later
-        socket_info.sin_addr.s_addr = htonl(INADDR_ANY); // 0.0.0.0
+        sockaddr_in* socket_info = new sockaddr_in;
+        socket_info->sin_family = AF_INET;
+        socket_info->sin_port = htons(12345); // Change later
+        socket_info->sin_addr.s_addr = htonl(INADDR_LOOPBACK); // 127.0.0.1
         // Binding listening socket
-        if ( bind(master_socket, (struct sockaddr *)&socket_info, sizeof(socket_info)) == -1 ) {  // Question here!!!
+        if ( bind(master_socket, (sockaddr *)socket_info, sizeof(*socket_info)) == -1 ) {
+            delete socket_info;
             throw Error_types::Bind_error(&master_socket);
         }
         std::cout << "Listening socket has been binded!\n";
+        delete socket_info;
 
         // Setting listening socket to listening
         if ( listen(master_socket, SOMAXCONN) == -1 ) {
